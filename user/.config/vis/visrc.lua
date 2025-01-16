e89feb58ca6690abaed0ea3628071781d38974fc
TahaGTRC <tahamedhous@gmail.com>
2025-01-16 08:57:55 +0100

    [add] Adding a papercolor theme to vis

--- user/.config/vis/visrc.lua
+++ user/.config/vis/visrc.lua
@@ -3,11 +3,12 @@ require('vis')
 
 local highlight = require('highlight-comment')
 highlight.keywords = {
-	NOCOMMIT  = 'fore:cyan,underlined,bold,blink',
-	FIXME     = 'fore:red,underlined,bold',
-	NOTE      = 'fore:green,underlined,bold',
-	TODO      = 'fore:magenta,underlined,bold',
-	IMPORTANT = 'fore:yellow,underlined,bold',
+    NOCOMMIT  = 'fore:cyan,underlined,bold,blink',
+    FIXME     = 'fore:red,underlined,bold',
+    NOTE      = 'fore:green,underlined,bold',
+    TODO      = 'fore:magenta,underlined,bold',
+    IMPORTANT = 'fore:yellow,underlined,bold',
+    BUG       = 'fore:red,bold,reverse'
 }
 
 
@@ -18,11 +19,10 @@ end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win) -- luacheck: no unused args
 	-- Your per window configuration options e.g.
-	vis:map(vis.modes.NORMAL, '<C-v>', ':set layout v<Enter>')
-	vis:map(vis.modes.NORMAL, '<C-h>', ':set layout h<Enter>')
-	vis:map(vis.modes.NORMAL, 'Y','\"+y<Enter>')
-	vis:map(vis.modes.VISUAL, 'Y','\"+y<Enter>')
-	vis:map(vis.modes.VISUAL_LINE, 'Y','\"+y<Enter>')
+	vis:map(vis.modes.NORMAL,		'<C-v>',	':set layout v<Enter>')
+	vis:map(vis.modes.NORMAL,		'<C-h>',	':set layout h<Enter>')
+	vis:map(vis.modes.VISUAL,		'Y',		'"+y')
+	vis:map(vis.modes.VISUAL_LINE,	'Y',		'"+y')
 	vis:command('set number')
 	vis:command('set rnu on')
 	vis:command('set ai off')
